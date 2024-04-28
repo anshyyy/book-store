7edf82d46ef904e964aaa8bc1ec98789bff30ea5
Anshuman Sharma <anshuman9998@gmail.com>
2024-04-28 09:22:37 +0530

    bill c

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const userControllers = require("../controllers/userControllers");
 const bookController = require("../controllers/bookController");
 const issueController = require("../controllers/issueController");
 const notificationController = require("../controllers/notificationController");
+const billController = require("../controllers/billController");
 app.get("/hello",(req,res)=>{
     res.send("hello")
 });
@@ -12,5 +13,6 @@ app.use("/user",userControllers);
 app.use("/book",bookController);
 app.use("/issue",issueController);
 app.use("/notify",notificationController);
+app.use("/bill",billController);
 
 module.exports = app;
