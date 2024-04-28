779b4275b8f89fe3305e2189630ee6dfe932368e
Anshuman Sharma <anshuman9998@gmail.com>
2024-04-28 09:20:18 +0530

    bill

--- src/controllers/billController.js
+++ src/controllers/billController.js
@@ -4,33 +4,38 @@ const Bill = require("../models/bill");
 const Issue = require("../models/issue");
 const User = require("../models/user");
 
-app.post("/",async(req,res)=>{
-    try{
-        const {user_id,data,subscritpion_id,amount,discount} = req.body;
-        data.forEach(async (element)=>{
-             if(element.typeOf=="issue"){
-                await Issue.create({
-                    bookId : element.bookId,
-                    assignedTo : user_id,
-                    returnDate : Date(element.returnDate)
-                });
-             } else if(element.typOf == "subscription"){
-                    await User.updateOne({
-                        id : user_id
-                    },{
-                       isPremium : true,
-                       lastDate : element.lastDate
-                    })
-             } 
+app.post("/", async (req, res) => {
+    try {
+        const { user_id, data, subscritpion_id, amount, discount } = req.body;
+        var today = new Date();
+
+        // Get the date 7 days from today
+        var seventhDayFromToday = new Date(today);
+        seventhDayFromToday.setDate(today.getDate() + 7);
+        data?.issue.forEach(async (element) => {
+            console.log(element);
+            await Issue.create({
+                bookId: element.bookId,
+                assignedTo: data.user_id,
+                returnDate: seventhDayFromToday
+            });
+            //  } else if(element.typOf == "subscription"){
+            //         await User.updateOne({
+            //             id : user_id
+            //         },{
+            //            isPremium : true,
+            //            lastDate : element.lastDate
+            //         })
+            //  } 
         });
 
         const bill = await Bill.create({
-            userId: user_id,
-            data:data,
-            subscription_id:subscritpion_id,
-            amount:amount,
-            discount:discount,
-            paid : true
+            userId: data.user_id,
+            data: data,
+            subscription_id: subscritpion_id??"",
+            amount: amount,
+            discount: discount??0,
+            paid: true
         });
         return res.status(200).json({
             msg: "SuccessFully created the bill",
@@ -38,11 +43,11 @@ app.post("/",async(req,res)=>{
             success: true
         })
 
-    } catch(error){
+    } catch (error) {
         return res.status(501).json({
-            msg : "Something went wrong",
-            err : error.message ?? error,
-            success : false
+            msg: "Something went wrong",
+            err: error.message ?? error,
+            success: false
         });
     }
 })
